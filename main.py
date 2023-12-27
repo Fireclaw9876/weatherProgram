97ecd545f2bd8f448ec543cc0bb2c999f583b922
Fireclaw9876 <41933596+Fireclaw9876@users.noreply.github.com>
2023-12-27 00:43:08 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+# Import Libraries
+import requests
+from bs4 import BeautifulSoup
+
+def weather(city):
+    # Empty List to input into CSV File
+    list = []
+
+    # For the Query
+    city = city.replace(" ", "+")
+
+    link = "https://www.google.com/search?q=weather+" + city
+    res = requests.get(link)
+    # Website Title
+    soup = BeautifulSoup(res.content, "html.parser")
+
+    # #specify to soup.find() method is the HTML tag name and the matched attributes
+    # temperature = soup.select('#wob_tm')[0].getText().strip()
+
+    element = soup.find('div', class_='BNeawe iBp4i AP7Wnd')
+
+    # If the element is found, .text is used to extract the text within the span element.
+    if element:
+        extracted_text = ""
+        # only extract the number
+        for letter in element.text:
+            if letter.isdigit():
+                extracted_text += letter
+
+        return(extracted_text)  # Output: 54
+
+def convertCelsius(num):
+    celsius = round((num - 32) * 5/9)
+    return str(celsius)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Enter City Here: ")
+    # input city
+    city = str(input())
+
+    print(weather(city) + "° Fahrenheit")
+
+    print("Would You Like To Covert to Celsius? ")
+    # yes or no
+    response = str(input())
+    if response.isalpha():
+        for letter in response:
+            if letter == 'y' or letter == 'Y':
+                print(convertCelsius(int(weather(city))) + "° Celsius")
+
+    print("Done Computing")
